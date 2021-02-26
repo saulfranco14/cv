e1a9b9ac6502dbfae530df9c26023da7f0801b88
Saul Franco <saul.franco@tucanton.com>
2021-02-25 23:27:07 -0600

    Se actualiza mi cv

---
+++ cv/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Fragment } from 'react';
+import GenerateCoding from '../src/components/GenerateCoding.component';
+
+function App() {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+}
+
+export default App;
